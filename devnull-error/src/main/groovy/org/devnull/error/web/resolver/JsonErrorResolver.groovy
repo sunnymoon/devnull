b1bca18fb22232e597fb546674848dce8c87e551
Mike Cantrell <mike@devnull.org>
2012-11-05 21:01:00 -0600

    explicit rendering of json HttpErrorMessages instead of using reflection

--- devnull-error/src/main/groovy/org/devnull/error/web/resolver/JsonErrorResolver.groovy
+++ devnull-error/src/main/groovy/org/devnull/error/web/resolver/JsonErrorResolver.groovy
@@ -1,28 +1,37 @@
 package org.devnull.error.web.resolver
 
 import groovy.json.JsonBuilder
-
+import org.devnull.error.web.message.HttpErrorMessageConverter
 import org.springframework.web.servlet.HandlerExceptionResolver
 import org.springframework.web.servlet.ModelAndView
 
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-import org.devnull.error.web.message.HttpErrorMessageConverter
 
 /**
  * Renders HttpErrorMessages as JSON to HTTP Clients
  */
 class JsonErrorResolver implements HandlerExceptionResolver {
 
-  HttpErrorMessageConverter httpErrorMessageConverter
+    HttpErrorMessageConverter httpErrorMessageConverter
 
-  ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-    def error = httpErrorMessageConverter.convert(ex, request)
-    response.status = error.statusCode
-    def writer = new OutputStreamWriter(response.outputStream)
-    new JsonBuilder(error).writeTo(writer)
-    writer.close()
-    return new ModelAndView()
-  }
+    ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        def error = httpErrorMessageConverter.convert(ex, request)
+        response.status = error.statusCode
+        def writer = new OutputStreamWriter(response.outputStream)
+        def json = new JsonBuilder()
+        json {
+            requestUri(error.requestUri)
+            date(error.date.time)
+            user(error.user)
+            statusCode(error.statusCode)
+            messages(error.messages)
+            fieldMessages(error.fieldMessages)
+            stackTrace(error.stackTrace)
+        }
+        json.writeTo(writer)
+        writer.close()
+        return new ModelAndView()
+    }
 
 }
